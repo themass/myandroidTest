cf6c95a2973dca20377a40ab01649106b0dbb16a
liguoqing <liguoqing@lianjia.com>
2017-01-04 16:49:48 +0800

    New changelist

---
+++ app/src/main/java/com/timeline/vpn/ui/log/FileLogActivity.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012 Tobias Brunner
+ * Hochschule fuer Technik Rapperswil
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ */
+
+package com.timeline.vpn.ui.log;
+
+import android.os.Bundle;
+
+import com.timeline.vpn.R;
+import com.timeline.vpn.ui.base.BaseBannerAdsActivity;
+
+public class FileLogActivity extends BaseBannerAdsActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_activity);
+        setToolbarTitle(R.string.menu_btn_bug);
+    }
+}
